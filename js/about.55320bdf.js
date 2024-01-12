2dc23a35da524ddc73a6520d5013ae69bd9098b5
spatrik2001 <scheuer.patrik@gmail.com>
2024-01-12 19:07:25 +0100

    Updates

--- js/about.55320bdf.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkzsormc"]=self["webpackChunkzsormc"]||[]).push([[443],{833:function(n,r,s){s.r(r),s.d(r,{default:function(){return l}});var u=s(3396);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),c=[a];function e(n,r){return(0,u.wg)(),(0,u.iD)("div",t,c)}var o=s(89);const i={},f=(0,o.Z)(i,[["render",e]]);var l=f}}]);
-//# sourceMappingURL=about.55320bdf.js.map
---
+++ js/about.e60a864f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkzsormc"]=self["webpackChunkzsormc"]||[]).push([[443],{833:function(n,r,s){s.r(r),s.d(r,{default:function(){return l}});var u=s(6252);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),c=[a];function e(n,r){return(0,u.wg)(),(0,u.iD)("div",t,c)}var o=s(3744);const i={},f=(0,o.Z)(i,[["render",e]]);var l=f}}]);
+//# sourceMappingURL=about.e60a864f.js.map
