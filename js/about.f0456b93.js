8ac10b5050a9b4c72533d4500c6bc20f51b41d76
spatrik2001 <scheuer.patrik@gmail.com>
2024-01-12 18:36:30 +0100

    Updates

--- js/about.f0456b93.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkzsormc"]=self["webpackChunkzsormc"]||[]).push([[443],{833:function(n,r,s){s.r(r),s.d(r,{default:function(){return l}});var u=s(252);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),c=[a];function e(n,r){return(0,u.wg)(),(0,u.iD)("div",t,c)}var o=s(744);const i={},f=(0,o.Z)(i,[["render",e]]);var l=f}}]);
-//# sourceMappingURL=about.f0456b93.js.map
